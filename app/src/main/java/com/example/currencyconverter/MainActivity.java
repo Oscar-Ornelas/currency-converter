deffac83ea70abd0937c20595993b5f54a502b1e
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-07-04 17:30:08 -0500

    Add euro conversion onClick method

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        double exchangeRate = 1.12;
+        EditText euroEditText = (EditText) findViewById(R.id.euroEditText);
+
+        double dollarAmount = Math.round((Double.parseDouble(euroEditText.getText().toString()) * exchangeRate) * 100.0) / 100.0;
+
+        Toast.makeText(this, String.valueOf(dollarAmount), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
